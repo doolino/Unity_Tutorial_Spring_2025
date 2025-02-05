50e5b4cf01737bff4c22bbbe075c94c8147799b8
Iman <109436129+ImanNoferesti@users.noreply.github.com>
2025-02-05 07:14:32 -0600

    Added Week 2

---
+++ Week2/InClassDemo/Demo/Program.cs
@@ -0,0 +1,138 @@
+﻿
+//////////////////// Arrays /////////////////////////
+
+// Declaration
+
+// Initialization
+
+// Declaration and Initialization Together
+
+// Alternative Short Syntax
+
+// Accessing and Modifying Elements
+
+// Get the size of an array
+
+// Sort scores
+
+// Copy the first 2 elements of scores to a new array
+
+// Copy scores starting at index 1 in destinationArray
+
+// Find the first even score 
+
+// Find all scores greater than 10
+
+// Find the index of the first occurrence of the score 15.
+
+// Find the index of the first occurrence of the score greater than 5
+
+// Check whether score of 20 exists
+
+// Clear the last two scores
+
+
+
+//////////////////// Lists /////////////////////////
+
+// Declare and initialize a list
+
+// Accessing elements
+
+// Get the list capacity
+
+// Get the length (count) of elements in the list
+
+// Add values to the list
+
+// Add a multiple values to the list at once
+
+// Check whether the list contains an element
+
+// Insert 110 before the last element
+
+// Insert -20, 10, and 0 at the start of the list
+
+// Remove the first occurrence of the value -20 from the list
+
+// Remove the value at index 0 from the list
+
+// Remove all elements greater than 50
+
+// Sort the list
+
+// Convert the list into an array of the same type
+
+// Copy the list values into an array of double size, starting from index 5
+
+// Check whether at least an odd value exists
+
+// Find the first even score 
+
+// Find all values greater than 10
+
+// Find the index of the first occurrence of the value 15.
+
+// Find the index of the first occurrence of the value greater than 5
+
+// Remove all elements from the list, making it empty
+
+
+//////////////////// Dictionaries /////////////////////////
+
+// Declare and Initialize a dictionary
+
+// Add elements
+
+// Accessing a key and Modifying its value
+
+// Show the total number of students
+
+// Retrieve all keys
+
+// Retrieve all values
+
+// Retrieve all key-value pairs
+
+// Check whether a key exists. If so, show its value
+
+// Remove a key
+
+// Remove all keys and values
+
+
+//////////////////// HashSets /////////////////////////
+
+// Declare and Initialize a HashSet
+
+// Add elements
+
+// Remove an element
+
+// Check if an element exists
+
+// Iterate through hashset
+
+// Copy all elements into an array
+
+// Remove all elements in the HashSet
+
+// Combine two sets 
+
+// Intersect two sets
+
+// Difference between two sets
+
+// Symmetric Difference between two sets (elements that are only in one)
+
+
+//////////////////// Functions /////////////////////////
+
+// Write a function that takes two integers and return their sum.
+// Then, call the function
+
+
+// Write a void function that prints messages on console
+
+
+// Write a function that greet users with an optional parameter
---
+++ Week2/InClassPractice/Practice/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Problem #1
+ * Create a program that manages a student's scores. The program should:
+ * Allow adding a single score or multiple scores at once.
+ * Display the total number of scores stored and check if a specific score exists.
+ * Find the first score above a given threshold and all scores below a given threshold.
+ * Remove the lowest score.
+ * Provide an option to clear all scores.
+ */
+
+
+
+
+
+
+/* Problem #2
+ * Create a program that manages a student gradebook, where each student's name is associated with their GPA. Implement the following functionalities:
+ * AddStudent: Adds a new student and their GPA to the gradebook. If the student already exists, display a message saying they are already added.
+ * RemoveStudent: Removes a student from the gradebook if they exist.
+ * CheckStudent: Checks if a student exists in the gradebook.
+ * PrintGradebook: Displays all student names and their GPAs.
+ * ShowStatistics: Displays the total number of students.
+ * ClearGradebook: Removes all students from the gradebook.
+ */
+ 
+ 
+ 
+
+
+
+/* Problem #3
+ * Write a function that removes the smallest value from a given list of integers in place (modifying the original list).
+ * Test your function
+ */
+
+
+
+
+
+/* Problem #4
+ * Write a function that takes an integer value and an array of integers, then returns how many times the given value appears in the array.
+ */
+ 
+ 
+ 
+ 
