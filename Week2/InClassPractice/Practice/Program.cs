764cdddf61d9389416ec394ab4e69fd84b304192
Iman <109436129+ImanNoferesti@users.noreply.github.com>
2025-02-13 12:13:42 -0600

    Edited some problems InClassPractice

--- Week2/InClassPractice/Practice/Program.cs
+++ Week2/InClassPractice/Practice/Program.cs
@@ -1,10 +1,11 @@
 ﻿/* Problem #1
- * Create a program that manages a student's scores. The program should:
- * Allow adding a single score or multiple scores at once.
- * Display the total number of scores stored and check if a specific score exists.
+ * Create a program that manages a student's scores. 
+ * Try adding a single score and multiple scores at once.
+ * Display the total number of scores stored.
+ * Check if a specific score exists.
  * Find the first score above a given threshold and all scores below a given threshold.
  * Remove the lowest score.
- * Provide an option to clear all scores.
+ * Clear all scores.
  */
 
 
@@ -13,10 +14,10 @@
 
 
 /* Problem #2
- * Create a program that manages a student gradebook, where each student's name is associated with their GPA. Implement the following functionalities:
+ * Create a program that manages a student gradebook, where each student's name is associated with their GPA.
+ * Initialize the gradebook with some names and grades.
  * AddStudent: Adds a new student and their GPA to the gradebook. If the student already exists, display a message saying they are already added.
  * RemoveStudent: Removes a student from the gradebook if they exist.
- * CheckStudent: Checks if a student exists in the gradebook.
  * PrintGradebook: Displays all student names and their GPAs.
  * ShowStatistics: Displays the total number of students.
  * ClearGradebook: Removes all students from the gradebook.
--- Week2/InClassPractice/Practice/obj/Debug/net9.0/Practice.AssemblyInfo.cs
+++ Week2/InClassPractice/Practice/obj/Debug/net9.0/Practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e009b848e460774d3718aeaa13cd49c0c780301e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b74530b70097fb4102f44bd293e61273a4dd60bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
