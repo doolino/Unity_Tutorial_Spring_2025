9103cc4e9ae2e79790f62a06b8264b56cd123657
Aditya <avprakash98@gmail.com>
2025-02-20 17:18:33 -0600

    .

--- Week3-4/InClassDemo/src/obj/Debug/net9.0/src.AssemblyInfo.cs
+++ Week3-4/InClassDemo/src/obj/Debug/net9.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3906adbcaaf5ae61147582c485c7381008caca3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3e02235c7d1e29d4081487a8abe779e99a84159")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
